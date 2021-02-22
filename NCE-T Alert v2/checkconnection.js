14c627b7e88dc4c4a9b40ec07448c4772ae9a748
YuraFilionchik <ura_f@mail.ru>
2021-02-22 19:26:23 +0200

    add files via upload
    added input into popup.html for selecting update interval

---
+++ NCE-T Alert v2/checkconnection.js
@@ -0,0 +1,23 @@
+﻿let init_interval=3000;
+var interval=init_interval;
+let audio=new Audio();
+audio.src=chrome.runtime.getURL('Warning.mp3');
+setInterval(function() {
+let alertbox=document.getElementById('offline');
+//console.log("checking...");
+if(alertbox!=null) {
+    interval=init_interval/2;
+    audio.play();
+} else interval=init_interval;
+document.getElementById('interval').textContent=interval;
+},interval);
+
+function OnBtnClick()
+{
+    let input=document.getElementById('interval');
+    console.log('input='+input.value);
+    let int=Number.parseInt(input.value);
+    if(!int || int <100) return;
+    console.log('set interval='+int);
+   interval=int;
+}
